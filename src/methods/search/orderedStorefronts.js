da1c494b93846c28e62ed2423150e3cda7e022f8
Anna Choinska-Hasbi <achoinska-hasbi@xogrp.com>
2020-02-05 13:56:18 -0500

    answers

--- src/methods/search/orderedStorefronts.js
+++ src/methods/search/orderedStorefronts.js
@@ -2,4 +2,16 @@
 'use strict';
 
 module.exports = (order, storefronts) => { 
+   // order = [1,6, 3]
+  // storefronts =[ {id: "3"}, {id: "6"}, {id: "1"},  ]
+  //  
+
+  //  find storefront that is in order, 
+  // add to front of array
+  // then add rest of items
+
+  order.map(store => storefronts.find(storefront => storefront.id === store.id))
+
+  //
+
 };
